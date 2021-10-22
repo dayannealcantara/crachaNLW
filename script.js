1690bad3e44fa45c54007a686aa799a03511af8b
Dayanne Alcantara <dayannealcantara14@gmail.com>
2021-10-22 11:21:04 -0300

    Pegando informções diretas do github

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ const links = {
   twitter: "DayanneAlcanta2",
   instagram: "dayanneal",
 };
+
+function getGitHubInfos () {
+  const url = `https://api.github.com/users/${links.github}`
+
+  fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    userName.textContent = data.name
+    userBio.textContent = data.bio
+    userLink.href = data.html_url
+    userImagem.src = data.avatar_url
+    userLogin.textContent = data.login
+  })
+}
+
+getGitHubInfos()
